fca8a18901e617c8e7881b3bb4c2164dff2caeec
ehakansoy <63318320+ehakansoy@users.noreply.github.com>
2025-08-26 22:02:25 +0100

    Update Home.jsx

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,11 +1,9 @@
 function Home() {
     return (
         <div style={{ textAlign: 'center' }}>
-            Not sure how you ended here,
-            <br/>
-            but I don't have 3D vision :(
+            ocean breathe rhythm.
             <br/><br/>
-            The trips section is optimized for mobile view - I didn't really support desktop view
+            <i>Trips section is optimized for mobile view.</i>
         </div>
     );
 }
